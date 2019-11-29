58ce18b11f72418e8eb8c4a9a746457e4218f732
arnabbhat82 <arnabbhat@gmail.com>
2019-11-29 12:39:32 +0530

    Material Icon

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,15 @@ import { MaterialModule } from './material/material.module';
 import { TypographyComponent } from './typography/typography.component';
 import { ButtonComponent } from './button/button.component';
 import { ButtonToggleComponent } from './button-toggle/button-toggle.component';
+import { IconsComponent } from './icons/icons.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TypographyComponent,
     ButtonComponent,
-    ButtonToggleComponent
+    ButtonToggleComponent,
+    IconsComponent
   ],
   imports: [
     BrowserModule,
